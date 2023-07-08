fcf8546c8e5b419408cfe719d8c64d359a4b4770
Daniel MacPhee <macpheed3@gmail.com>
2023-07-08 13:10:56 -0400

    add input map, add dynamic portraits

--- conversation.gd
+++ conversation.gd
@@ -7,25 +7,51 @@ var dialogueIndex = 0
 var dialogues = [
 	{
 		"speaker": "Prince Gog",
-		"portrait": "default",
+		"portrait": "gog_default",
 		"message": "Blather blather blather this is test dialogue don't read too much into it, no big deal I am after all a prince."
+	},
+	{
+		"speaker": "Prince Gog",
+		"portrait": "gog_angry",
+		"message": "Now I'm angry!!!"
 	}
 ]
 
 
 func _ready():
 	displayNextMessage()
-	pass
 
+
+func isFinishedTyping() -> bool:
+	return $SpeechBorder/SpeechBG/Speech.visible_characters >= $SpeechBorder/SpeechBG/Speech.text.length()
 
 func displayNextMessage():
-	$Speech.text = dialogues[dialogueIndex].message
-	$Speaker.text = "[center]" + dialogues[dialogueIndex].speaker + "[/center]"
+	var dialogue = dialogues[dialogueIndex]
+	var portrait = load("res://assets/" + dialogue.portrait + ".png")
+	$SpeakerPortrait.set_texture(portrait)
+	$SpeechBorder/SpeechBG/Speech.visible_characters = 0
+	$SpeechBorder/SpeechBG/Speech.text = dialogue.message
+	$SpeakerBorder/SpeakerBG/Speaker.text = "[center]" + dialogue.speaker + "[/center]"
 
 	if $TypeTimer.is_stopped():
 		$TypeTimer.start()
 
 
+func advanceMessage():
+	dialogueIndex += 1
+	displayNextMessage()
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("text_advance"):
+		if isFinishedTyping():
+			advanceMessage()
+		else:
+			$SpeechBorder/SpeechBG/Speech.visible_characters = $SpeechBorder/SpeechBG/Speech.text.length()
+
+
 func _on_type_timer_timeout():
-	if $Speech.visible_characters < $Speech.text.length():
-		$Speech.visible_characters += 1
+	if not isFinishedTyping():
+		$SpeechBorder/SpeechBG/Speech.visible_characters += 1
+	else:
+		$TypeTimer.stop()
