6add65806965a17d4b6c89504390904d288e6985
STOBtheRob <robert.tyler.steele@gmail.com>
2023-07-08 13:50:28 -0400

    add menu (it works)

---
+++ mainmenu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _ready():
+	$FadeIn.play("Fade_In")
+
+func _on_play_pressed():
+	$SelectUIAudio.play()
+	$FadeOut.play("Fade_Out")
+	await get_tree().create_timer(2).timeout
+	get_tree().change_scene_to_file("res://game.tscn")
+	
+	
+func _on_exit_pressed():
+	$SelectUIAudio.play()
+	$FadeOut.play("Fade_Out")
+	await get_tree().create_timer(2).timeout
+	get_tree().quit()
+	
