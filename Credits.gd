7eb621e4a03db88e4519c47a9179145f5e423581
STOBtheRob <robert.tyler.steele@gmail.com>
2023-07-09 03:07:53 -0400

    fixing credits

--- Credits.gd
+++ Credits.gd
@@ -5,3 +5,7 @@ extends Node2D
 func _ready():
 	$MainTheme.play()
 	$ScrollAction.play("Text_Scroll")
+	await get_tree().create_timer(38).timeout
+	$MainTheme.play()
+	await get_tree().create_timer(38).timeout
+	get_tree().quit()
