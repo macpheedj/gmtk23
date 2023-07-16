a64a1ffa2d5e1fb9794b7d6eb44402485d8da0b4
STOBtheRob <robert.tyler.steele@gmail.com>
2023-07-16 12:04:38 -0400

    Added Dossier functionality in the background

--- Dossier.gd
+++ Dossier.gd
@@ -4,12 +4,18 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$FadeIn.play("Fade_In")
-	
+	var ratfile = FileAccess.open("user://ratdossier.txt", FileAccess.READ)
+	$RatNotes.text = ratfile.get_as_text()
+	var mantisfile = FileAccess.open("user://mantisdossier.txt", FileAccess.READ)
+	$MantisNotes.text = mantisfile.get_as_text()
+	var horsefile = FileAccess.open("user://horsedossier.txt", FileAccess.READ)
+	$HorseNotes.text = horsefile.get_as_text()
+	var frogfile = FileAccess.open("user://frogdossier.txt", FileAccess.READ)
+	$FrogNotes.text = frogfile.get_as_text()
 
 
 func _on_rat_profile_mouse_entered():
 	$SelectUISound.play()
-	
 
 
 func _on_horse_profile_mouse_entered():
@@ -25,5 +31,20 @@ func _on_frog_profile_mouse_entered():
 
 
 func _on_back_button_pressed():
+	var ratfile = FileAccess.open("user://ratdossier.txt", FileAccess.WRITE)
+	ratfile.store_string($RatNotes.text)
+	
+	var mantisfile = FileAccess.open("user://mantisdossier.txt", FileAccess.WRITE)
+	mantisfile.store_string($MantisNotes.text)
+	
+	var horsefile = FileAccess.open("user://horsedossier.txt", FileAccess.WRITE)
+	horsefile.store_string($HorseNotes.text)
+	
+	var frogfile = FileAccess.open("user://frogdossier.txt", FileAccess.WRITE)
+	frogfile.store_string($FrogNotes.text)
+	
 	$BackUISound.play()
-	get_tree().change_scene_to_file("res://mainmenu.tscn")
+	get_tree().change_scene_to_file("res://BufferTest.tscn")
+
+
+
