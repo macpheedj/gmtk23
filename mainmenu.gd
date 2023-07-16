88353141eb1f7604064235bcdd6b4716a2b9540a
STOBtheRob <robert.tyler.steele@gmail.com>
2023-07-16 10:12:57 -0400

    Added main menu Credit sbutton and mouse hover

--- mainmenu.gd
+++ mainmenu.gd
@@ -10,7 +10,16 @@ func _on_play_pressed():
 	$FadeOut.play("Fade_Out")
 	await get_tree().create_timer(2).timeout
 	get_tree().change_scene_to_file("res://DatingPhase1.tscn")
-
+	
+func _on_play_mouse_entered():
+	$MouseHoverAudio.play()
+	
+	
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://Credits.tscn")
+	
+func _on_credits_mouse_entered():
+	$MouseHoverAudio.play()
 	
 	
 func _on_exit_pressed():
@@ -19,7 +28,21 @@ func _on_exit_pressed():
 	await get_tree().create_timer(2).timeout
 	get_tree().quit()
 	
+func _on_exit_mouse_entered():
+	$MouseHoverAudio.play()
 
 
 func _on_title_float_animation_finished(_anim_name):
 	$TitleFloat.play("Title_Float")
+
+
+
+
+
+
+
+
+
+
+
+
