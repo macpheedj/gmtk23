52684ae625e3627c8e4d671f46f944bcf05261e1
Daniel MacPhee <macpheed3@gmail.com>
2023-07-23 17:49:11 -0400

    state update works

--- state.gd
+++ state.gd
@@ -1,30 +1,39 @@
 # script to handle game state
+extends Node
+
+const DEBUG = true
 
 enum Princess { Chessa, Sienna, Wilhelmina }
 enum Category { Trivia, Location }
-enum State { Known, Unknown }
+enum Knowledge { Known, Unknown }
 
 var princess_states: Dictionary = {
-    [Princess.Chessa]: {
-        [Category.Trivia]: State.Unknown,
-        [Category.Location]: State.Unknown,
-    },
-    [Princess.Sienna]: {
-        [Category.Trivia]: State.Unknown,
-        [Category.Location]: State.Unknown,
-    },
-    [Princess.Wilhelmina]: {
-        [Category.Trivia]: State.Unknown,
-        [Category.Location]: State.Unknown,
-    },
+	Princess.Chessa: {
+		Category.Trivia: Knowledge.Unknown,
+		Category.Location: Knowledge.Unknown,
+	},
+	Princess.Sienna: {
+		Category.Trivia: Knowledge.Unknown,
+		Category.Location: Knowledge.Unknown,
+	},
+	Princess.Wilhelmina: {
+		Category.Trivia: Knowledge.Unknown,
+		Category.Location: Knowledge.Unknown,
+	},
 }
 
 func get_state():
-    return princess_states
+	return princess_states
 
-func set_state(princess: Princess, category: Category, state: State):
-    if not princess is Princess: return
-    if not category is Category: return
-    if not state is State: return
+func set_state(princess: Princess, category: Category):
+	if not princess is Princess: return
+	if not category is Category: return
 
-    princess_states[princess][category] = state
+	if DEBUG:
+		print("[State] updating state")
+		print("[State] Princess: " + str(Princess.keys()[princess]))
+		print("[State] Category: " + str(Category.keys()[category]))
+		print("")
+
+	# nothing is ever gonna go from known -> unknown, so this can be a constant
+	princess_states[princess][category] = Knowledge.Known
